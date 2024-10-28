087066f836b02bb9328aebc582d1bb145376d042
Chinmaya0011 <imchinu17@gmail.com>
2024-10-28 23:52:27 +0530

    simple

---
+++ pratice.py
@@ -0,0 +1,14 @@
+# a=41
+# b=5
+# c=6
+# if(a>b and a>c):
+#     print("A is greater")
+# elif(b>a and b>c):
+#     print("B is Greater")
+# else:
+#     print("C is Greater")
+num=77
+if(num%7==0):
+    print("Divisible")
+else:
+    print("No Divisible")
